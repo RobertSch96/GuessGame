dd711b46648bcaa1678417d452c1016f21b95d73
RobertSch96 <107917307+RobertSch96@users.noreply.github.com>
2022-10-05 10:17:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # This is a guess the number game
-from multiprocessing.sharedctypes import Value
 import random
+import string
 import sys
 import _tkinter
+from tokenize import Number
 import streamlit as st
 
 st.text('Thanks for Trying!')
@@ -12,32 +13,65 @@ st.header('Guess The Number')
 st.subheader('Have fun!')
 
 
-st.write("Hello, what is your name?")
-name = st.text_input(label="Your Name Here")
+st.write("Hello friend!")
+if "disabled" not in st.session_state:
+    st.session_state["disabled"] = False
 
-st.write("Well " + name + " i am thinking of a number between 1- 20")
-#secretnumber = random.randint(1,20)
-st.write("Take a guess")
+def disable():
+    st.session_state["disabled"] = True
 
-#def ggame(a):
+name  = st.text_input(
+    "What is your name?", 
+    disabled=st.session_state.disabled, 
+    on_change=disable
+)
 
-secretnumber = random.randint(1,20)
+if name:
+    st.write("Well " + name + " i am thinking of a number between 1- 20")
+    st.write("Take a guess")
+
+Coin = 25
+st.write("you have " + str(Coin) + " Coins!")
+
 lol = random.randint(1,200)
-a = st.number_input("number goes here", key=lol)
-if a > secretnumber:
-    st.write("Your number is greater than 20!")
-elif a == secretnumber:
-    st.write('You guessed correctly!')
-    st.write("The Secret Number was " +str(secretnumber))
-elif a != secretnumber:
-    if a > secretnumber:
-      st.write('Incorrect! Your guess was too high!')
-      st.write("Take another guess!")
-elif a < secretnumber:
-    st.write("Incorrect! Your guess was too low")
-    st.write("Take another guess!")
-            
 
+
+a = st.number_input("Number between 1-20",value=0)
+if Coin == 0:
+     st.write("Youre out of Coins! Please purchase more Coins")
+
+
+def game(Coin):
+    Attempts = 6
+    secretnumber = random.randint(1,20)
+    for Attempts in range(1-7):       
+        if a == 0:
+            st.write("")               
+        elif a != secretnumber:
+            if a > secretnumber:
+                st.write('Incorrect! Your guess was too high!')
+                st.write("Take another guess!")
+                Attempts = Attempts - 1
+                st.write("you have " + str(Attempts)+ " Attempts left")
+                Coin = Coin-1
+                st.write("You have " + str(Coin) + " Coins left")
+            elif a < secretnumber:
+                    st.write("Incorrect! Your guess was too low")
+                    st.write("Take another guess!")
+                    Attempts = Attempts - 1
+                    st.write("you have " + str(Attempts)+ " Attempts left")
+                    Token = Coin-1
+                    st.write("You have " + str(Coin) + " Coins left")
+        else:
+                st.write('You guessed correctly!')
+                st.write("The Secret Number was " +str(secretnumber))
+                st.write("you had " + str(Attempts)+ " Attempts left")
+                Token = Coin + 5
+                st.write("You have " + str(Coin) + " Coins left")
+                break
+        return Coin
+            
+Coin = game(a)
    
 
 
